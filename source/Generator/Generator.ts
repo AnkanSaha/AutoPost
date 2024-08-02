c2d54afd49515ddac486ac5e0b4037ece2e22fdc
Ankan Saha <ankansahadaluabari@gmail.com>
2024-08-02 11:44:06 +0530

    chore: Update GenerateTopic function prompt
    - Update the prompt in the GenerateTopic function to include specific topics related to MERN stack, MongoDB, Nodejs, Docker, Express, React, Redux, Javascript, Typescript, Ci/Cd, and general software development topics.

--- source/Generator/Generator.ts
+++ source/Generator/Generator.ts
@@ -2,7 +2,7 @@
 import GeminiGenerateContent from "../Ai/Gemini"
 
 export default async function GenerateTopic(): Promise<string | string[]> {
-    const prompt = `Write a javascript Array of topics for linkedin post ideas for a software developer, i need just 2 topics in simple array format, not objects or anything fancy, just simple array of strings, i just only array no text or markdown or anything else, just array of strings, You must pick the topics from MERN stack, MongoDB, Nodejs, Docker, Express, React, Redux,, Javascript, Typescript, Ci/Cd or all relevant topic that match with them or similar to them, you can also include some general topics like software development, software engineering etc`
+    const prompt = `Write a javascript Array of topics for linkedin post ideas for a software developer, i need just 2 topics in simple array format, not objects or anything fancy, just simple array of strings, i just only array no text or markdown or anything else, just array of strings, You must pick the topics from MERN stack, MongoDB, Nodejs, Docker, Express, React, Redux,, Javascript, Typescript or all relevant topic that match with them or similar to them, you can also include some general topics like software development, software engineering etc this all must be unique`
 
     const response = String(await GeminiGenerateContent(prompt))
     return JSON.parse(response); // Return the topics
@@ -11,7 +11,7 @@ export default async function GenerateTopic(): Promise<string | string[]> {
 
 
 export async function GenerateContentTags(topic: string): Promise<string| string[]> {
-    const prompt = `Write a javascript Array of tags for linkedin post ideas for a software developer, i need just 3 tags in simple array format, in simple one line text, not dot or not objects or anything fancy, just simple array of strings, i just only array no text or markdown or anything else, just array of strings, You must pick the tags from ${topic} stack, MongoDB, Nodejs, Docker, Express, React, Redux,, Javascript, Typescript, Ci/Cd or all relevant topic that match with them or similar to them, you can also include some general tags like software development, software engineering etc in utf-8 format only & don't include non-alphanumeric or prohibited unicode characters don't give me tags with spaces or special characters, just simple tags in utf-8 format`
+    const prompt = `Write a javascript Array of tags for linkedin post ideas for a software developer, i need just 3 tags in simple array format, in simple one line text, not dot or not objects or anything fancy, just simple array of strings, i just only array no text or markdown or anything else, just array of strings, You must pick the tags from ${topic} stack, MongoDB, Nodejs, Docker, Express, React, Redux,, Javascript, Typescript or all relevant topic that match with them or similar to them, you can also include some general tags like software development, software engineering etc in utf-8 format only & don't include non-alphanumeric or prohibited unicode characters don't give me tags with spaces or special characters, just simple tags in utf-8 format`
 
     const response = String(await GeminiGenerateContent(prompt));
     return JSON.parse(response); // Return the tags
@@ -26,7 +26,7 @@ export async function GenerateFullPost(): Promise<string | object[]> {
 
     // Generate Content for each topic
     for (const singleTopic of topics) {
-            const prompt = `Write a linkedin post for a software developer about ${singleTopic}`
+            const prompt = `Write a linkedin post for about ${singleTopic}`
             const tags = await GenerateContentTags(singleTopic);
             const response = await GeminiGenerateContent(prompt);
             fullPostDetails.push({
